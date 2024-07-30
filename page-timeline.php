5b6399bea40b2371ebc869f30331338e5597e387
Joel Salisbury <joel.saisbury@gmail.com>
2024-07-29 20:44:37 -0400

    page template and initial styles.

---
+++ page-timeline.php
@@ -0,0 +1,37 @@
+<?php
+
+/** Timeline Template, page-timeline.php */
+get_header(); ?>
+
+<div class="timeline-wrap container mt-4 timeline">
+
+    <div class="timeline-element circle-orange-green left">
+        <div class="timeline-photo">
+            <img class="timeline-photo" src="img/husky-1.jpg" width="100%" height="200px" alt="husky">
+            <p class="timeline-tag tag-orange">A STRONGER, MORE INCLUSIVE COMMUNITY<sup>2</sup><span class="tooltiptext">Identify and pursue new revenue opportunities, including industry partnerships and joint ventures that expand technology commercialization and startup creation.</span></p>
+            <p class="timeline-tag-2 tag-green">WELLNESS OF PEOPLE AND PLANET<sup>3</sup><span class="tooltiptext">Recruit diverse faculty and staff and strengthen strategies that increase their retention.</span></p>
+        </div>
+        <div class="timeline-details">
+            <small class="timeline-date">05/28/24</small>
+            <h3>Helping Teachers Recognize Gifted Learners and Elevate Classroom Conversation</h3>
+            <p>'Project Focus' brings more learners into gifted programs — and more strategies from gifted education into general classrooms.</p>
+            <a href="#" class="btn btn-default">CONTINUE READING</a>
+        </div>
+    </div>
+
+    <div class="timeline-element circle-blue right">
+        <div class="timeline-photo">
+            <img class="timeline-photo" src="img/husky-2.jpg" width="100%" height="200px" alt="husky">
+            <p class="timeline-tag tag-blue">HUSKY PRIDE AND RESILIENCE<sup>4</sup><span class="tooltiptext">Translate and disseminate UConn's impactful research that improves the human experience and contributes to philanthropy, innovation, and entrepreneurship.</span></p>
+        </div>
+        <div class="timeline-details">
+            <small class="timeline-date">05/20/24</small>
+            <h3>UConn Health Selected for Duffy Status Health Equity Project</h3>
+            <p>UConn Health has been selected among 24 health and hospital systems for the American Society of Hematology project to establish new blood reference ranges that better reflect diverse patient populations.</p>
+            <a href="#" class="btn btn-default">CONTINUE READING</a>
+        </div>
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
