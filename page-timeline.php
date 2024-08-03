0642d6470980e0a3c015edae610312078e1c0e70
Joel Salisbury <joel.saisbury@gmail.com>
2024-08-03 11:27:59 -0400

    revising the prioirity actions are working

--- page-timeline.php
+++ page-timeline.php
@@ -3,6 +3,28 @@
 /** Timeline Template, page-timeline.php */
 get_header(); ?>
 
+
+    <div class="row">
+        <div class="col-md-12 text-center" style="margin-top: 50px;">
+            <h1 class="text-white"> Lede Content and Filters</h1>
+            <p class="text-white"> This is the lede content for the timeline page. It can be edited in the page-timeline.php file.</p>
+            <!-- BUTTON GROUP -->
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-secondary">All</button>
+                <button type="button" class="btn btn-secondary">Husky Pride and Resilience</button>
+                <button type="button" class="btn btn-secondary">A Stronger, More Inclusive Community</button>
+                <button type="button" class="btn btn-secondary">Wellness of People and Planet</button>
+                <button type="button" class="btn btn-secondary">Excellence in Research, Innovation, and Engagement
+                </button>
+                <button type="button" class="btn btn-secondary">Seven World-Class Campuses, One Flagship University
+                </button>
+                <button type="button" class="btn btn-secondary">Student Success Journey</button>
+
+
+            </div>
+        </div>
+    </div>
+
 <div class="timeline-wrap mt-4 timeline">
 
     <!-- <div class="timeline-element circle-orange-green left">
@@ -45,7 +67,7 @@ get_header(); ?>
 
     if ($timeline_items->have_posts()) {
         while ($timeline_items->have_posts()) {
-        
+
             $timeline_items->the_post();
             // get the featured image
             $timeline_photo = get_the_post_thumbnail_url(get_the_ID(), 'full');
@@ -54,7 +76,11 @@ get_header(); ?>
             // the tags are custom taxonomy area-of-focus
             $terms = get_the_terms($post->ID, 'area-of-focus');
             foreach ($terms as $term) {
-                $areas_of_focus[] = $term->name;
+                $areas_of_focus[$term->slug] = array(
+                    'name' => $term->name,
+                    'slug' => $term->slug,
+                    'color' => get_field('color', $term)
+                );
             }
            
             $timeline_date = get_the_date();
@@ -63,17 +89,37 @@ get_header(); ?>
             // external_link is a custom field
             $timeline_link = get_field('external_link');
 
-            $timeline_tag_1 = $areas_of_focus[0];
-            $timeline_tag_2 = $areas_of_focus[1];
+            $priority_action_text = get_field('priority_action_text');
+
+
+            
         ?>
 
-            <div class="timeline-element circle-orange-green <?php echo ($count % 2 == 0) ? 'left' : 'right'; ?>">
+            <div class="timeline-element
+            <?php 
+            // if there are two areas of focus, add the class hyphenating the two colors, otherwise just the one
+            if (count($areas_of_focus) > 1) {
+                echo 'circle-' . $areas_of_focus[array_key_first($areas_of_focus)]['color'] . '-' . $areas_of_focus[array_key_last($areas_of_focus)]['color'];
+            } else {
+                echo 'circle-' . $areas_of_focus[array_key_first($areas_of_focus)]['color'];
+            }
+            ?>
+            ">
                 <div class="timeline-photo">
                     <img class="timeline-photo" src="<?php echo $timeline_photo; ?>" width="100%" height="200px" alt="husky">
-                    <p class="timeline-tag tag-orange"><?php echo $timeline_tag_1; ?></p>
-                    <?php if ($timeline_tag_2) { ?>
-                        <p class="timeline-tag-2 tag-green"><?php echo $timeline_tag_2; ?></p>
-                    <?php } ?>
+                    <?php
+                    $count = 1;
+                    foreach ($areas_of_focus as $area) {
+                        
+                    ?>
+                        <p class="timeline-tag tag-<?php echo $area['color']; ?>
+                        <?php echo "timeline-tag-" . $count++; ?>
+                        "><?php echo $area['name']; ?>
+                            <span class="tooltiptext"><?php echo $priority_action_text; ?></span>
+                        </p>
+                    <?php
+                    }
+                    ?>
                 </div>
                 <div class="timeline-details">
                     <small class="timeline-date"><?php echo $timeline_date; ?></small>
